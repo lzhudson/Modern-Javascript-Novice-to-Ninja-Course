bb42e4ee89fbe059ca68c521c9fe2c8006e8a3cf
Hudson Holanda <hudsonholanda55@gmail.com>
2019-07-18 17:30:58 -0300

    Adding files related to variables class, constants and comments.

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,13 @@
+let myAge = 18;
+let year = 2019;
+// log things to console
+console.log(age, year);
+/*
+age  = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);*/
+
+var score = 75;
+console.log(score); 
