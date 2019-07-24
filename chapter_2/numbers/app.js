9f2c967783aad94ccdb44344917254b8f1099cdb
Hudson Holanda <hudsonholanda55@gmail.com>
2019-07-24 17:10:58 -0300

    adding files from class numbers

---
+++ chapter_2/numbers/app.js
@@ -0,0 +1,35 @@
+let radius = 10;
+const pi = 3.14;
+
+//console.log(radius, pi);
+
+// math operator +, -, /, *, **, %
+
+//console.log(10 / 2);
+//et result = radius % 3;
+
+//let result = pi * radius**2;
+
+//order of operation - B I D M A S
+
+//let result = 5 * (10 - 3)**2;
+
+//console.log(result);
+
+let likes = 10;
+
+// likes = likes + 1;
+// likes++;
+//likes--;
+//likes += 10;
+//likes -= 5;
+//likes *= 2;
+//likes /= 2;
+//console.log(likes);
+
+// NaN - not a number
+// console.log(5 / 'hello');
+// console.log(5 * '/');
+
+let result = 'the blog has ' + likes + ' likes';
+console.log(result);
