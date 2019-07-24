7770ed9abd6d1620db1db8110aec074b5e9efc71
Hudson Holanda <hudsonholanda55@gmail.com>
2019-07-24 17:08:25 -0300

    adding files form class boolean and comparisions

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,7 +1,7 @@
 let myAge = 18;
 let year = 2019;
 // log things to console
-console.log(age, year);
+console.log(myAge, year);
 /*
 age  = 30;
 console.log(age);
@@ -9,5 +9,11 @@ console.log(age);
 const points = 100;
 console.log(points);*/
 
-var score = 75;
 console.log(score); 
+
+const points = 100;
+
+console.log(points);
+
+var score = 75;
+console.log(score);
