c8f1123116303fabc708e0b1b1d78d82c9c0bce5
Hudson Holanda <hudsonholanda55@gmail.com>
2019-07-24 17:10:13 -0300

    adding files from class lose and strict comparison

---
+++ chapter_2/lose & strict comparison/app.js
@@ -0,0 +1,14 @@
+let age = 25;
+
+// lose comparison (differente types can still be equal)
+
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log(age != 25);
+// console.log(age != '25');
+
+// strict comparison (differente types cannot be equal)
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25');
