668dd770790920006a971b07861427f45e54e482
Hudson Holanda <hudsonholanda55@gmail.com>
2019-07-24 17:09:46 -0300

    adding files from class string methods common

---
+++ chapter_2/common string methods/app.js
@@ -0,0 +1,13 @@
+// common string methods
+let email = 'hudsonholanda55@gmail.com';
+
+//let result = email.lastIndexOf('n');
+//let result = email.slice(0, 10);
+
+//let result = email.substr(4, 10);
+
+//let result = email.replace('m', 'w');
+
+let result = email.replace('n', 'w');
+
+console.log(result);
