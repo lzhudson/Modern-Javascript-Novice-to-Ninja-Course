8162e91e0cbdf19d003263b9b7df29f6cf510df6
Hudson Holanda <hudsonholanda55@gmail.com>
2019-07-25 12:43:03 -0300

    adding files from class if statements

---
+++ chapter_3/if statements/app.js
@@ -0,0 +1,16 @@
+// if statements
+// const age = 25;
+// if(age > 20){
+//   console.log('you are over 20 years old');
+// }
+// const ninjas = ['shuan', 'ryu', 'chun-li', 'yoshi'];
+
+// if(ninjas.length > 3){
+//   console.log('that a lot ninjas');
+// }
+
+const password = 'password';
+
+if(password.length >= 8){
+  console.log('that password is long enough!');
+}
