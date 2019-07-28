ec4617762d5373f8fd492f3ea439498ac0ef4d57
Hudson Holanda <hudsonholanda55@gmail.com>
2019-07-28 18:22:08 -0300

    adding files from class Arrow Functions.

---
+++ chapter_4/arrow functions/practise arrow functions/app.js
@@ -0,0 +1,19 @@
+// practise arrow functions 
+// const greet = function(){
+//   return 'hello, world';
+// }
+
+const greet = () => 'hello, world';
+const result = greet();
+console.log(result);
+
+greet();
+
+const bill = (products, tax) => {
+  let total = 0;
+  for(let i = 0; i < products.length; i++){
+    total += products[i] + products[i] * tax;
+  }
+  return total;
+}
+console.log(bill([10,15,30], 0.2));
