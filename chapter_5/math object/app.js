019aa8425d5eb4f83416fb8ab02f4a358df5541b
Hudson Holanda <hudsonholanda55@gmail.com>
2019-07-30 09:50:54 -0300

    adding files from math objects class

---
+++ chapter_5/math object/app.js
@@ -0,0 +1,21 @@
+// math object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+const testeFloor = 5.3;
+const testeCeil = 10.90;
+const testeTrunc = 20.45;
+
+console.log(Math.round(area));
+console.log(Math.floor(testeFloor));
+console.log(Math.ceil(testeCeil));
+console.log(Math.trunc(testeTrunc));
+
+//random numbers
+
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random * 100));
