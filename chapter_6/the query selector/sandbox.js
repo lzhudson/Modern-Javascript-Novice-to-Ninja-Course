b7f59ea90dd99bd18adb80d7f86cfcde1755bce4
Hudson Holanda <hudsonholanda55@gmail.com>
2019-07-31 19:17:11 -0300

    adding files from the query selector class

---
+++ chapter_6/the query selector/sandbox.js
@@ -0,0 +1,13 @@
+// const para = document.querySelector('div.error');
+
+// console.log(para);
+
+const paras = document.querySelectorAll('p');
+const erros = document.querySelectorAll('.error');
+
+
+paras.forEach(para =>{
+  console.log(para);
+})
+
+console.log(erros);
