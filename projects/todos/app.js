b7103a75278f03533fe01d686fbfabd7d41f3249
Hudson Holanda <hudsonholanda55@gmail.com>
2019-09-10 11:18:27 -0300

    refatorando codigo.

--- projects/todos/app.js
+++ projects/todos/app.js
@@ -3,45 +3,50 @@ const formSearch = document.querySelector('.search');
 const inputSearch = formSearch.search;
 const inputAddTodo = formAdd.add;
 const iconDelete = document.querySelectorAll('.delete');
-const todos = document.querySelectorAll('.todos li');
+const todos = document.querySelector('.todos');
+const list = document.querySelectorAll('.todos li');
 const messsageError = document.querySelector('.invalid-feedback');
-
+function createElementTodo(textTodo){
+  const ul = document.querySelector('.todos');
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const icon = document.createElement('i');
+  span.innerText = textTodo;
+  li.classList.add('list-group-item','d-flex', 'justify-content-between', 'align-items-center');
+    icon.classList.add('far', 'fa-trash-alt','delete');
+    li.appendChild(span);
+    li.appendChild(icon);
+    ul.appendChild(li);
+}
 function addTodo(e) { 
   e.preventDefault();
   var regex = /^\w+$/gmi;
   if(regex.test(inputAddTodo.value)){
     messsageError.style.display = '';
-    const ul = document.querySelector('.todos');
-    const li = document.createElement('li');
-    const span = document.createElement('span');
-    const icon = document.createElement('i');
-    span.innerText = inputAddTodo.value;
-    li.classList.add('list-group-item','d-flex', 'justify-content-between', 'align-items-center');
-    icon.classList.add('far', 'fa-trash-alt','delete');
-    li.appendChild(span);
-    li.appendChild(icon);
-    ul.appendChild(li);
+    createElementTodo(inputAddTodo.value);
     formAdd.reset();
   } else {
-    messsageError.style.display = 'block';
-    inputAddTodo.classList.add('.input-error'); 
+   showMessageError();
   }
 }
-function removeTodo(e){
-  e.target.parentElement.remove();
+function showMessageError(){
+  messsageError.style.display = 'block';
+  inputAddTodo.classList.add('input-error');
+  setTimeout(()=>{
+    messsageError.style.display = 'none';
+    inputAddTodo.classList.remove('input-error');
+  }, 5000)
 }
 formAdd.addEventListener('submit', addTodo);
-iconDelete.forEach((element)=>{
-  element.addEventListener('click', removeTodo);
-});
+
 const filterTodos = (term)=>{
-  Array.from(todos).filter((todo) => {
+  Array.from(list).filter((todo) => {
     return !todo.textContent.toLowerCase().includes(term);
   }).forEach((todo)=>{
     todo.classList.add('filtered');
   })
   
-  Array.from(todos).filter((todo) => {
+  Array.from(list).filter((todo) => {
     return todo.textContent.toLowerCase().includes(term);
   }).forEach((todo)=>{
     todo.classList.remove('filtered');
@@ -50,4 +55,9 @@ const filterTodos = (term)=>{
 inputSearch.addEventListener('keyup', () =>{
   const term = inputSearch.value.trim().toLowerCase();
   filterTodos(term);
+});
+todos.addEventListener('click', (e) =>{
+  if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+  }
 })
